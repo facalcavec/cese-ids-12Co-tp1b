7bb5e1cf17e061a921a38b52e46386aa80de8061
Felipe Calcavecchia <facalcavec@gmail.com>
2020-07-04 18:53:46 -0300

    Se agregan los datos del alumno CALCAVECCHIA, Felipe

--- src/alumnos.c
+++ src/alumnos.c
@@ -31,8 +31,15 @@ static const struct alumno_s ESTEBAN_VOLENTINI = {
     .documento = "23.517.968",
 };
 
+static const struct alumno_s FELIPE_CALCAVECCHIA = {
+        .apellidos = "CALCAVECCHIA",
+        .nombres = "Felipe Alberto",
+        .documento = "17.163.886",
+    };
+
 const alumno_t ALUMNOS[] = {
     &ESTEBAN_VOLENTINI,
+    &FELIPE_CALCAVECCHIA,
 };
 
 const int CANTIDAD_ALUMNOS = (sizeof(ALUMNOS) / sizeof(alumno_t));
